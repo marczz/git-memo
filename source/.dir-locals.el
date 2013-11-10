ed401b270a0ed2bb6117d80b127afee713620d53
Marc Zonzon <marc.zonzon+git@gmail.com>
2013-11-10 19:33:47 +0100

    .dir-locals.el: new file - dir local variables for rst-mode

---
+++ source/.dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((rst-mode
+  (rst-indent-width . 4)
+  (rst-indent-field . 4)
+  (rst-indent-literal-normal . 4)
+  (rst-indent-comment . 4)
+  (ispell-local-dictionary . "english")
+))
